361e60f0519c359a3d898eef5dd5fd1995c21e4d
PinkyHi <ivanvladymyrov.pinky@gmail.com>
2019-11-06 15:27:13 +0200

    buyMenu

--- eAdvertisement_bot/Models/Commands/BuyMenuCommand.cs
+++ eAdvertisement_bot/Models/Commands/BuyMenuCommand.cs
@@ -29,15 +29,22 @@ namespace eAdvertisement_bot.Models.Commands
 
         public override async Task Execute(Update update, TelegramBotClient botClient)
         {
-            AppDbContext dbContext = new AppDbContext();
-            DbEntities.User userEntity = dbContext.Users.Find(update.Message.From.Id);
-            long hold = dbContext.Advertisements.Where(a => a.User_Id==userEntity.User_Id && a.Advertisement_Status_Id == 1).Sum(a=>a.Price);
-            await botClient.AnswerCallbackQueryAsync(update.CallbackQuery.Id, null, false);  // ...,...,alert    AnswerCallbackQuery is required to send to avoid clock animation ob the button
+            try
+            {
+                AppDbContext dbContext = new AppDbContext();
+                DbEntities.User userEntity = dbContext.Users.Find(update.CallbackQuery.Message.Chat.Id); 
+                long hold = dbContext.Advertisements.Where(a => a.User_Id == userEntity.User_Id && a.Advertisement_Status_Id == 1).Sum(a => a.Price);
+                await botClient.AnswerCallbackQueryAsync(update.CallbackQuery.Id, null, false);  // ...,...,alert    AnswerCallbackQuery is required to send to avoid clock animation ob the button
 
-            InlineKeyboardMarkup keyboard = buyMenuKeyboard;
+                InlineKeyboardMarkup keyboard = buyMenuKeyboard;
 
-            await botClient.EditMessageTextAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, "Balance: "+userEntity.Balance+"\nHold: "+hold);
-            await botClient.EditMessageReplyMarkupAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, keyboard);
+                await botClient.EditMessageTextAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, "Balance: " + userEntity.Balance + "\nHold: " + hold);
+                await botClient.EditMessageReplyMarkupAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, keyboard);
+            }
+            catch(Exception ex)
+            {
+
+            }
         }
     }
 }
